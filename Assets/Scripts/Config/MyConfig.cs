b743d16a79e4f0cf1d057e69164f02e5881934b1
Sze Wa, Wong <szewa.wong@connect.polyu.hk>
2020-10-12 11:18:49 +0800

    added MyConfig

---
+++ Assets/Scripts/Config/MyConfig.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace Fila3dColouring.Config
+{
+    [CreateAssetMenu(menuName = "Fila3dColouring/Config/My Config")]
+    public class MyConfig : ScriptableObject
+    {
+        #region constants
+
+        private const string FileName = "config.json";
+        private const string FolderPath = "Data/";
+
+        #endregion
+
+
+        [SerializeField]
+        private bool isLoadFromFile;
+
+
+        #region ScriptableObject hooks
+
+        private void Awake()
+        {
+            
+        }
+
+        #endregion
+
+
+        private void LoadFromFile()
+        {
+
+        }
+    }
+}
